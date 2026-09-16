e4cc50ec0457136f6e256d95704c4165d9318f0f
CryptonFuture <crypton.futuremedia1989@gmail.com>
2026-09-16 09:05:44 +0500

    frontend

--- blood_backend/src/server.js
+++ blood_backend/src/server.js
@@ -8,10 +8,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL || "https://bloodfrontend-six.vercel.app",
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/auth'));
